25aa86efca9e88a34e86066ad384677fe2543d81
Chiwon Cho <goldworm@icon.foundation>
2019-05-29 00:20:03 +0900

    Initial project

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os import environ, path
+
+version = environ.get("VERSION")
+
+if version is None:
+    with open(path.join(".", "VERSION")) as version_file:
+        version = version_file.read().strip()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="gw-iconsdk",
+    version=version,
+    description="Python version IconSDK made by goldworm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="ICON foundation",
+    author_email="foo@icon.foundation",
+    url="https://github.com/goldworm-icon/gw-iconsdk",
+    packages=find_packages(exclude=["tests*"]),
+    test_suite="tests",
+    install_requires=[
+         "eth-keyfile>=0.5.1",
+         "secp256k1>=0.13.2",
+         "multipledispatch>=0.5.0",
+         "requests>=2.20.0"
+    ],
+    setup_requires=["pytest-runner"],
+    tests_requires=["pytest"],
+    license="Apache License 2.0",
+    classifiers=[
+         "Development Status :: 5 - Production/Stable",
+         "Intended Audience :: Developers",
+         "Intended Audience :: System Administrators",
+         "Natural Language :: English",
+         "License :: OSI Approved :: Apache Software License",
+         "Programming Language :: Python",
+         "Programming Language :: Python :: 3.6"
+         "Programming Language :: Python :: 3.7"
+     ]
+)
