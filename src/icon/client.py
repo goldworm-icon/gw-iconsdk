49b56dec74d728d668aff6ccac14e02444abd6b5
Chiwon Cho <goldworm@icon.foundation>
2020-08-08 17:54:07 +0900

    Arrange codes in Client class

--- src/icon/client.py
+++ src/icon/client.py
@@ -13,27 +13,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict
+from typing import Dict, Any
 
-from .builder.generic_builder import GenericBuilder
 from .builder.method import Method
 from .data.address import Address
 from .data.block import Block
 from .data.rpc_request import RpcRequest
+from .data.rpc_response import RpcResponse
 from .data.transaction import Transaction
 from .data.transaction_result import TransactionResult
 from .provider.provider import Provider
-from .utils.convert_type import str_to_int, bytes_to_hex
+from .utils.convert_type import str_to_int, bytes_to_hex, hex_to_bytes
 
 
 class Client(object):
-    def __init__(self, provider: Provider):
+    def __init__(self, provider: Provider, defaults: Dict[str, Any] = None):
+        if defaults is None:
+            defaults = {}
+        elif not isinstance(defaults, dict):
+            raise ValueError(f"Invalid params: defaults={defaults}")
+
         self._provider = provider
+        self._defaults = defaults
+
+    @property
+    def defaults(self) -> Dict[str, Any]:
+        return self._defaults
 
     def get_block_by_hash(self, block_hash: bytes) -> Block:
         params = {"txHash": bytes_to_hex(block_hash)}
-        request = RpcRequest(Method.GET_BLOCK_BY_HASH, params)
-        response = self._provider.send(request)
+        response = self._send(Method.GET_BLOCK_BY_HASH, params)
         return Block.from_dict(response.result)
 
     def get_block_by_height(self, block_height: int) -> Block:
@@ -41,51 +50,45 @@ class Client(object):
             raise ValueError(f"Invalid params: {block_height}")
 
         params = {"txHash": hex(block_height)}
-        request = RpcRequest(Method.GET_BLOCK_BY_HASH, params)
-        response = self._provider.send(request)
+        response = self._send(Method.GET_BLOCK_BY_HASH, params)
         return Block.from_dict(response.result)
 
     def get_transaction(self, tx_hash: bytes) -> Transaction:
         params = {"txHash": bytes_to_hex(tx_hash)}
-        request = RpcRequest(Method.GET_TRANSACTION_BY_HASH, params)
-        response = self._provider.send(request)
+        response = self._send(Method.GET_TRANSACTION_BY_HASH, params)
         return Transaction.from_dict(response.result)
 
     def get_transaction_result(self, tx_hash: bytes) -> TransactionResult:
-        builder = GenericBuilder(Method.GET_TRANSACTION_RESULT)
-        builder.add("txHash", tx_hash)
-        params: Dict[str, str] = builder.build()
-
-        request = RpcRequest(Method.GET_TRANSACTION_RESULT, params)
-        response = self._provider.send(request)
+        params = {"txHash": bytes_to_hex(tx_hash)}
+        response = self._send(Method.GET_TRANSACTION_RESULT, params)
         return TransactionResult.from_dict(response.result)
 
     def get_total_supply(self) -> int:
-        request = RpcRequest(Method.GET_TOTAL_SUPPLY)
-        response = self._provider.send(request)
+        response = self._send(Method.GET_TOTAL_SUPPLY)
         return str_to_int(response.result)
 
     def get_balance(self, address: Address) -> int:
-        request = RpcRequest(Method.GET_BALANCE, {"address": str(address)})
-        response = self._provider.send(request)
+        params = {"address": str(address)}
+        response = self._send(Method.GET_BALANCE, params)
         return str_to_int(response.result)
 
     def get_score_api(self, address: Address) -> Dict[str, str]:
-        request = RpcRequest(Method.GET_SCORE_API, {"address": str(address)})
-        response = self._provider.send(request)
+        params = {"address": str(address)}
+        response = self._send(Method.GET_SCORE_API, params)
         return response.result
 
-    def send_transaction(self, params: Dict[str, str] = None):
-        request = RpcRequest(Method.SEND_TRANSACTION, params)
-        response = self._provider.send(request)
-        return response
+    def send_transaction(self, params: Dict[str, str] = None) -> bytes:
+        response = self._send(Method.SEND_TRANSACTION, params)
+        return hex_to_bytes(response.result)
 
-    def call(self, params: Dict[str, str]):
-        request = RpcRequest(Method.CALL, params)
-        response = self._provider.send(request)
+    def call(self, params: Dict[str, str]) -> Dict[str, str]:
+        response = self._send(Method.CALL, params)
         return response.result
 
     def estimate_step(self, params: Dict[str, str]) -> int:
-        request = RpcRequest(Method.ESTIMATE_STEP, params)
-        response = self._provider.send(request)
+        response = self._send(Method.ESTIMATE_STEP, params)
         return str_to_int(response.result)
+
+    def _send(self, method: str, params: Dict[str, str] = None) -> RpcResponse:
+        request = RpcRequest(method, params)
+        return self._provider.send(request)
